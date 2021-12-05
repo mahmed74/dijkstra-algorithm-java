6639febecac1f765c4cf1c316444a79c1f9e44fd
mahmed74 <ahmedsheikh0704@gmail.com>
2021-12-05 23:38:41 +0500

    gui updated

--- src/javaapplication12/Graphchart.java
+++ src/javaapplication12/Graphchart.java
@@ -40,11 +40,30 @@ public class Graphchart extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
         jComboBox1 = new javax.swing.JComboBox<>();
         jComboBox2 = new javax.swing.JComboBox<>();
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable3 = new javax.swing.JTable();
+        jLabel4 = new javax.swing.JLabel();
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -72,6 +91,42 @@ public class Graphchart extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 17)); // NOI18N
         jLabel1.setText("Dijkstra Algorithm for Path detection");
 
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel3.setText("Other ways");
+
+        jTable3.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {"A", "B",  new Integer(50)},
+                {"B", "C",  new Integer(60)},
+                {"A", "D",  new Integer(80)},
+                {"B", "C",  new Integer(60)},
+                {"B", "D",  new Integer(90)},
+                {"C", "E",  new Integer(40)},
+                {"D", "C",  new Integer(20)},
+                {"E", "B",  new Integer(50)}
+            },
+            new String [] {
+                "Point One", "Point Two", "Distance"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.Integer.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jTable3.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentShown(java.awt.event.ComponentEvent evt) {
+                jTable3ComponentShown(evt);
+            }
+        });
+        jScrollPane3.setViewportView(jTable3);
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel4.setText("Shortest Distance:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -79,6 +134,8 @@ public class Graphchart extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(65, 65, 65)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel3)
                     .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 281, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
@@ -86,8 +143,9 @@ public class Graphchart extends javax.swing.JFrame {
                         .addGap(52, 52, 52)
                         .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(47, 47, 47)
-                        .addComponent(jButton1)))
-                .addContainerGap(399, Short.MAX_VALUE))
+                        .addComponent(jButton1))
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 425, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(255, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -99,9 +157,15 @@ public class Graphchart extends javax.swing.JFrame {
                     .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
-                .addGap(50, 50, 50)
+                .addGap(30, 30, 30)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel2)
-                .addContainerGap(373, Short.MAX_VALUE))
+                .addGap(37, 37, 37)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 15, Short.MAX_VALUE)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(251, 251, 251))
         );
 
         pack();
@@ -132,7 +196,15 @@ public class Graphchart extends javax.swing.JFrame {
        thegraph.path();
         String textShown = "Shortest path from "+this.PointOne+" to "+this.PointTwo+" is " + this.thegraph.res;
         jLabel2.setText(textShown);
+       
+        
+      
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jTable3ComponentShown(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_jTable3ComponentShown
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jTable3ComponentShown
 
     /**
      * @param args the command line arguments
@@ -175,5 +247,11 @@ public class Graphchart extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable3;
     // End of variables declaration//GEN-END:variables
 }
--- src/javaapplication12/JavaApplication12.java
+++ src/javaapplication12/JavaApplication12.java
@@ -42,7 +42,8 @@ class Graph{
 
     private final int MAX_VERTS = 20;
     private final int INFINITY = 1000000;
-    private Vertex VertexList[]; // list of vertices
+//    private Vertex VertexList[]; // list of vertices
+    Vertex VertexList[];
     private int adjMat[][]; // adjacency matrix
     private int nVerts; // current number of vertices
     private int nTree; //number of vertices in tree
@@ -223,25 +224,25 @@ public class JavaApplication12 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-//        Graph theGraph = new Graph();
-//        theGraph.addVertex('A'); // 0 (Start)
-//        theGraph.addVertex('B'); // 1
-//        theGraph.addVertex('C'); // 2
-//        theGraph.addVertex('D'); // 3
-//        theGraph.addVertex('E'); // 4
-//        
-//        
-//        theGraph.addEdge(0, 1, 50); //AB 50
-//        theGraph.addEdge(0, 3, 80); // AD 80
-//        theGraph.addEdge(1, 2, 60); //BC 60
-//        theGraph.addEdge(1, 3, 90); // BD 90
-//        theGraph.addEdge(2, 4, 40); //CE 40
-//        theGraph.addEdge(3, 2, 20); // DC 20
-//        theGraph.addEdge(4,1,50); // EB 50
+        Graph theGraph = new Graph();
+        theGraph.addVertex("A"); // 0 (Start)
+        theGraph.addVertex("B"); // 1
+        theGraph.addVertex("C"); // 2
+        theGraph.addVertex("D"); // 3
+        theGraph.addVertex("E"); // 4
         
-//        System.out.println("Shortest paths");
-//        theGraph.path(); // shortest paths
-//        System.out.println();
+        
+        theGraph.addEdge(0, 1, 50); //AB 50
+        theGraph.addEdge(0, 3, 80); // AD 80
+        theGraph.addEdge(1, 2, 60); //BC 60
+        theGraph.addEdge(1, 3, 90); // BD 90
+        theGraph.addEdge(2, 4, 40); //CE 40
+        theGraph.addEdge(3, 2, 20); // DC 20
+        theGraph.addEdge(4,1,50); // EB 50
+        
+        System.out.println("Shortest paths");
+        theGraph.path(); // shortest paths
+        System.out.println();
         
     } // end main
      
